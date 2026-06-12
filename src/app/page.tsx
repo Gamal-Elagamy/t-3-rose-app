8a0815db068dabbd31973cd1ebc32f25e7f02bdb
Gamal-Elagamy <gamalelagmy82@gmail.com>
2026-06-12 22:31:06 +0300

    Initializing-project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className=" dark:bg-black">
+      <h1 className="text-3xl font-bold underline text-center text-gray-800 dark:text-white">
+        Home page
+      </h1>
+    </div>
+  );
+}
