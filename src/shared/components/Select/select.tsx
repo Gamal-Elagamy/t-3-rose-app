64121d53065cbdbd7febde8dcccfcf8c8c3e7cde
DavidNabil210 <davidnabil633@gmail.com>
2026-06-13 22:26:02 +0300

    feat: add Select component

---
+++ src/shared/components/Select/select.tsx
@@ -0,0 +1,39 @@
+'use client';
+
+interface SelectProps {
+  label?: string;
+  placeholder?: string;
+  options: { value: string; label: string }[];
+  disabled?: boolean;
+  onChange?: (value: string) => void;
+}
+
+const Select = ({
+  label,
+  placeholder = 'Select an option',
+  options,
+  disabled = false,
+  onChange,
+}: SelectProps) => {
+  return (
+    <div className="flex flex-col gap-1 w-full">
+      {label && <label className="text-sm font-medium ">{label}</label>}
+      <select
+        disabled={disabled}
+        onChange={(e) => onChange?.(e.target.value)}
+        className="w-full border border-border rounded-md px-3 py-2 text-sm bg-background text-foreground disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        <option value="" disabled selected>
+          {placeholder}
+        </option>
+        {options.map((opt) => (
+          <option key={opt.value} value={opt.value}>
+            {opt.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
