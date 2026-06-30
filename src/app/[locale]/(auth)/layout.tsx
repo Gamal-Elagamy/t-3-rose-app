1c1785f320d172f937ab6242e9c0d86e5c2f801a
Gamal-Elagamy <gamalelagmy82@gmail.com>
2026-06-30 22:51:34 +0300

    fix(auth): enable scrolling for auth layout

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -10,22 +10,18 @@ type AuthLayoutProps = {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <main className="h-screen overflow-hidden">
-      <div className="grid h-full grid-cols-1 md:grid-cols-2">
-        {/* Left Side */}
-        <section className="flex h-full flex-col bg-ds-bg-plain">
-          <AuthHeader />
+    <main className="h-screen overflow-hidden grid h-full grid-cols-1 md:grid-cols-2">
+      {/* Left Side */}
+      <section className="flex h-screen flex-col overflow-y-auto py-2 bg-ds-bg-plain">
+        <AuthHeader />
+        <div className="flex flex-1 items-center justify-center">{children}</div>
+        <AuthFooter />
+      </section>
 
-          <div className="flex flex-1 items-center justify-center">{children}</div>
-
-          <AuthFooter />
-        </section>
-
-        {/* Right Side */}
-        <aside className="relative hidden h-full md:block">
-          <Image src={imageUrl} alt="Authentication" fill priority className="object-cover" />
-        </aside>
-      </div>
+      {/* Right Side */}
+      <aside className="relative hidden h-full md:block">
+        <Image src={imageUrl} alt="Authentication" fill priority className="object-cover" />
+      </aside>
     </main>
   );
 }
