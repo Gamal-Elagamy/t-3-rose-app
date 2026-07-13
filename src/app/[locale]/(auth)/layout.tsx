7880fbb9e3b7acc40045e1b04cdd191e49fbbc3d
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2026-07-14 01:49:35 +0300

    fix(auth): fix confilct

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -10,9 +10,9 @@ type AuthLayoutProps = {
 
 export default function AuthLayout({ children }: AuthLayoutProps) {
   return (
-    <main className="grid h-screen grid-cols-1 overflow-hidden md:grid-cols-2">
+    <main className="grid h-screen grid-cols-1 overflow-hidden md:grid-cols-2 overflow-y-auto">
       {/* Left Side */}
-      <section className="flex flex-col overflow-y-auto bg-ds-bg-plain gap-10 min-w-96">
+      <section className="flex flex-col bg-ds-bg-plain gap-10 min-w-96">
         <AuthHeader />
 
         <div className="flex flex-1 items-center justify-center">{children}</div>
@@ -21,7 +21,7 @@ export default function AuthLayout({ children }: AuthLayoutProps) {
       </section>
 
       {/* Right Side */}
-      <aside className="relative hidden h-screen md:block">
+      <aside className="hidden md:block h-screen sticky top-0">
         <Image
           src={imageUrl}
           alt="Authentication"
