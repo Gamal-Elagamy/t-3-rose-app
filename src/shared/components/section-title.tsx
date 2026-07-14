53cfc29bce94a5460b5ad7d18c6939c877f8222d
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2026-07-14 06:00:09 +0300

    feat(home): implement section-title and section-small-title as reusable components.

---
+++ src/shared/components/section-title.tsx
@@ -0,0 +1,15 @@
+interface SectionTitleProps {
+  title: string;
+}
+
+export default function SectionTitle({ title }: SectionTitleProps) {
+  return (
+    <div className="relative w-fit">
+      <h2 className="relative z-10 text-4xl font-bold text-ds-text-primary">{title}</h2>
+
+      <div className="absolute -bottom-1 left-0 -z-10 h-4.5 w-3/4 rounded-r-full bg-soft-pink-100 dark:bg-ds-bg-plain">
+        <div className="absolute bottom-0 left-0 h-1 w-1/3 bg-soft-pink-600" />
+      </div>
+    </div>
+  );
+}
