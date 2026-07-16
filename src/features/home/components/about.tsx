648560d0dac7a18fd950c96ac1e8ba9f158d9ac7
Gamal-Elagamy <gamalelagmy82@gmail.com>
2026-07-16 15:50:50 +0300

    feat(home): complete about section implementation

--- src/features/home/components/about.tsx
+++ src/features/home/components/about.tsx
@@ -24,8 +24,12 @@ export default function About() {
       {/* Images */}
       <div className="flex h-auto w-1/2 gap-4">
         {/* Left Image */}
-        <div className="overflow-hidden max-w-80 max-h-96 rounded-4xl">
-          <Image src={gift1} alt="Gift box" />
+        <div className="relative mr-4 h-[344px] w-[302px]">
+          <div className="absolute rotate-[4deg] inset-0 rounded-[50px_120px_120px_120px] border-[4px] border-ds-border-primary" />
+
+          <div className="absolute inset-0 translate-x-6 translate-y-2 overflow-hidden rounded-[50px_120px_120px_120px]">
+            <Image src={gift1} alt="Gift Box" fill className="object-cover" />
+          </div>
         </div>
 
         {/* Right Images */}
@@ -33,8 +37,7 @@ export default function About() {
           <div className="overflow-hidden max-w-48 max-h-48 rounded-full">
             <Image src={gift2} alt="Gift box" />
           </div>
-
-          <div className=" mt-1 rounded-l-base overflow-hidden w-48 h-48 rounded-r-full ">
+          <div className="mt-1 h-[144px] w-[193px] overflow-hidden rounded-[50px_100px_100px_50px]">
             <Image src={gift3} alt="Gift box" className="h-full w-full object-cover" />
           </div>
         </div>
