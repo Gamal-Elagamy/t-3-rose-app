ab0960d9d5430b64f27ee6e6ccd46f6285cf0771
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2026-07-23 04:44:49 +0300

    feat(product): implement product image gallery.

--- src/app/[locale]/products/[id]/page.tsx
+++ src/app/[locale]/products/[id]/page.tsx
@@ -1,3 +1,20 @@
-export default function Product() {
-  return <div>Product</div>;
+import { getProduct } from '@/features/products/apis/products.api';
+import ProductImageGallery from '@/features/products/components/product-image-gallery';
+
+interface ProductPageProps {
+  params: Promise<{ id: string }>;
+}
+
+export default async function Product({ params }: ProductPageProps) {
+  const { id } = await params;
+  const product = await getProduct(id);
+
+  return (
+    <div className="max-w-11/12 mx-auto px-4 py-8">
+      {/* Product gallary & info */}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-17.5">
+        <ProductImageGallery product={product} />
+      </div>
+    </div>
+  );
 }
--- src/features/products/components/product-card.tsx
+++ src/features/products/components/product-card.tsx
@@ -48,7 +48,7 @@ export default function ProductCard({
 
   return (
     <div>
-      <div className="relative h-[300px] w-full rounded-2xl overflow-hidden">
+      <div className="relative h-75 w-full rounded-2xl overflow-hidden">
         <Link href={`/products/${id}`} className="block h-full relative">
           <Image
             src={cover}
