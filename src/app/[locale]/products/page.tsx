f09c7e81fe52a1940be4a999d5fd93b494f8bd15
DavidNabil210 <davidnabil633@gmail.com>
2026-07-31 01:50:33 +0300

    fix: address PR review comments on sidebar filters

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -1,11 +1,3 @@
-import { SidebarFilters } from '@/features/products/components/sidebar-filters/sidebar-filters';
-
 export default function Products() {
-  return (
-    <div className="flex gap-6">
-      <SidebarFilters />
-
-      <div className="flex-1">Products list...</div>
-    </div>
-  );
+  return <div>Products</div>;
 }
