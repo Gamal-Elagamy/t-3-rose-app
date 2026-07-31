c2be6d834e668fdee9aa033f4a4e3f88c46ffca1
Ziad El-Sayed <104182634+ZiadElsayed01@users.noreply.github.com>
2026-07-31 20:48:11 +0300

    fix(products): add suspense to product page.

--- src/app/[locale]/products/page.tsx
+++ src/app/[locale]/products/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react';
 import { SidebarFilters } from '@/features/products/components/sidebar-filters/sidebar-filters';
 
 export default async function ProductsPage() {
@@ -5,7 +6,9 @@ export default async function ProductsPage() {
     <section className="py-12">
       <div className="max-w-7xl mx-auto px-4">
         <div className="grid grid-cols-[300px_1fr] gap-6">
-          <SidebarFilters />
+          <Suspense fallback={<div className="w-72">Loading filters...</div>}>
+            <SidebarFilters />
+          </Suspense>
         </div>
       </div>
     </section>
