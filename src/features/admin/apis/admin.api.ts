3e42a889c41e66183037c7fb901ac29a3edcc984
Raheeemsala7 <rahemsalah534@gmail.com>
2026-08-24 01:30:45 +0300

    handle folders

--- src/features/admin/apis/admin.api.ts
+++
@@ -1,7 +0,0 @@
-import { getNextAuthToken } from "@/shared/lib/utils/auth.utils";
-
-export async function getAdminStatisticsApi() {
-    
-    const token = await getNextAuthToken()
-    if (token?.token) return RESPONSES 
-}
